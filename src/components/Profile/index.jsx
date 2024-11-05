fda925aedbcc2d8dd0a7ca8cee2a7c588603d299
Venicio duarte <151862005+VenicioDuarte@users.noreply.github.com>
2024-11-05 20:32:38 -0300

    Add files via upload

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.css'
+
+export default function Profile(props){
+    return(
+        <div className={styles.container}>
+            <img src={props.img} />
+            <h1>{props.name}</h1>
+            <hr />
+            <p>{props.bio}</p>
+            <hr />
+            <p>{props.phone}</p>
+            <hr />
+            <p>{props.email}</p>
+            <hr />
+            <button><a href={props.githubUrl}>Github</a></button>
+            <button><a href={props.InstagramUrl}>Instagram</a></button>
+            <button><a href={props.twitterUrl}>Twitter</a></button>
+        </div>
+    )
+}
