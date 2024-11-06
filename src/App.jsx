f14b690f555ad8b241e2574b782b302f43422499
Venicio duarte <151862005+VenicioDuarte@users.noreply.github.com>
2024-11-06 18:21:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Profile from "./components/Profile"
 export default function App(){
   return(
     <body>
-      <Profile img={'./public/vn.jpg'} name={'Venicio Duarte'} bio={'Dev. Python, 𝚃𝚎𝚌.𝚍𝚎𝚜𝚎𝚗𝚟𝚘𝚕𝚟𝚒𝚖𝚎𝚗𝚝𝚘 𝚍𝚎 𝚜𝚒𝚜𝚝𝚎𝚖𝚊.'} phone={'+ 55 (85) 99267971'} email={'gmotoplay23@gmail.com'} githubUrl={'https://github.com/VenicioDuarte'} InstagramUrl={'https://www.instagram.com/_venicio_duarte145/'}></Profile>
+      <Profile img={'./vn.jpg'} name={'Venicio Duarte'} bio={'Dev. Python, 𝚃𝚎𝚌.𝚍𝚎𝚜𝚎𝚗𝚟𝚘𝚕𝚟𝚒𝚖𝚎𝚗𝚝𝚘 𝚍𝚎 𝚜𝚒𝚜𝚝𝚎𝚖𝚊.'} phone={'+ 55 (85) 99267971'} email={'gmotoplay23@gmail.com'} githubUrl={'https://github.com/VenicioDuarte'} InstagramUrl={'https://www.instagram.com/_venicio_duarte145/'}></Profile>
     </body> 
   )
 }
